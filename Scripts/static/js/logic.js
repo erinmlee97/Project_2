9412d0497559315c20759df3632691d679b6232b
erinmlee97 <erinmlee97@gmailcom>
2020-10-14 15:50:28 -0400

    changes to index

--- Scripts/static/js/logic.js
+++ Scripts/static/js/logic.js
@@ -1,5 +1,10 @@
 var baseUrl = "https://swapi.dev/api/";
+var planetsUrl= "planets/";
+var starshipsUrl= "starships/";
+var speciesUrl= "species/";
+var vehicles= "vehicles/";
 
-d3.json(baseUrl, function(data){
+d3.json(baseUrl + planetsUrl).then(function(data){
     console.log(data)
 });
+
