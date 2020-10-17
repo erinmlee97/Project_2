f3c9cd6eb6223bf6ecc3bcf5587295819be6203a
John S <jswierc1@uncc.edu>
2020-10-17 08:53:31 -0400

    updating flask app

--- Scripts/static/js/logic.js
+++ Scripts/static/js/logic.js
@@ -1,5 +1,18 @@
 var baseUrl = "https://swapi.dev/api/";
+var planetsUrl = "planets/"
 
-d3.json(baseUrl, function(data){
-    console.log(data)
+
+d3.json(baseUrl + planetsUrl).then(function(data){
+    results = data.results;
+    var names = [];
+    var orb_periods = [];
+    for (let i = 0; i < results.length; i++) {
+        var name = results[i].name;
+        var orb_period = results[i].orbital_period;
+        names.push(name);
+        orb_periods.push(orb_period)
+    };
+    console.log(orb_periods);
+    
 });
+
--- Scripts/static/py/app.py
+++ Scripts/static/py/app.py
@@ -1,1 +1,31 @@
 from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+mongo = PyMongo(app, uri = "mongodb://localhost:27017/star_wars_app")
+
+@app.route("/")
+def home():
+
+    db_data = mongo.db.collection.find_one()
+    return render_template("index.html", star = db_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
