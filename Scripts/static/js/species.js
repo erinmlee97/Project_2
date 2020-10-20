b42e566dc91ac6a26e9657d639140cacc6b72b4c
carliemazar <azar.carlie@gmail.com>
2020-10-19 21:33:12 -0400

    potentially add a filter

--- Scripts/static/js/species.js
+++ Scripts/static/js/species.js
@@ -22,4 +22,46 @@ d3.csv('../data/species.csv').then(function(speciesData) {
         });
   }).catch(function(error) {        //catches errors
     console.log(error);
-  });
+//     // Add event listener to the button, with a function when clicked
+// $button.addEventListener("click", handleSearchButtonClick);
+
+// // renderTable outputs data into tbody
+// function renderTable() {
+//   $tbody.innerHTML = "";
+//   for (var i = 0; i < tableData.length; i++) {
+    
+//     var data = tableData[i];
+//     var inputs = Object.keys(data);
+   
+//     var row = $tbody.insertRow(i);
+//     for (var j = 0; j < inputs.length; j++) {
+//       var input = inputs[j];
+//       var $cell = row.insertCell(j);
+//       $cell.innerText = data[input];
+//     }
+//   }
+// }
+
+// function handleSearchButtonClick(event) {
+//   // prevent page from refreshing
+//   event.preventDefault();
+
+//   var DateFilter = $date.value.trim();
+//   if (DateFilter != "") {
+//     tableData = data.filter(function (data) {
+//       var DateField = data.datetime;
+//       return DateField === DateFilter;
+//     });
+// };
+// renderTable();
+//   }
+// function resetData() {
+//   tableData = data;
+//   $date.value = "";
+
+//   renderTable();
+// }
+
+// // Render the table on page load
+// renderTable();
+//   });
