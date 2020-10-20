369443b3c15adcd67cacdd49c9c8fa7871d6c2b6
elfulenw <elfulenw@gmail.com>
2020-10-20 16:24:59 -0400

    updates to starship data analysis

--- Scripts/static/js/starship.js
+++ Scripts/static/js/starship.js
@@ -1,12 +1,12 @@
-d3.csv('static/data/starships.csv').then(data => {
 
-    const root = partition(data);
-    
-    console.log(parseInt(d3.select('.pieDiv').style('width')));
-  
+var jsonData;
+
+d3.json('../static/js/flare.json').then(data => {
+  jsonData = data;
+    const root = partition(data); 
     width = parseInt(d3.select('.pieDiv').style('width'));
-
-    // root.each(d => d.current = d);
+    radius = width / 6
+    root.each(d => d.current = d);
   
 
 
@@ -20,84 +20,88 @@ d3.csv('static/data/starships.csv').then(data => {
   
     const path = g.append("g")
       .selectAll("path")
-      .data(root.descendants().slice(1))
+      .data(() => {
+        console.log(root);
+        return root.descendants().slice(1);
+      })
       .join("path")
-        .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
+        .attr("fill", d => { while (d.depth > 1) d = d.parent 
+          // return color(d.data.name); })
         .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
         .attr("d", d => arc(d.current));
   
-    // path.filter(d => d.children)
-    //     .style("cursor", "pointer")
-    //     .on("click", clicked);
+    path.filter(d => d.children)
+        .style("cursor", "pointer")
+        .on("click", clicked);
   
-    // path.append("title")
-    //     .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+    path.append("title")
+        .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
   
-    // const label = g.append("g")
-    //     .attr("pointer-events", "none")
-    //     .attr("text-anchor", "middle")
-    //     .style("user-select", "none")
-    //   .selectAll("text")
-    //   .data(root.descendants().slice(1))
-    //   .join("text")
-    //     .attr("dy", "0.35em")
-    //     .attr("fill-opacity", d => +labelVisible(d.current))
-    //     .attr("transform", d => labelTransform(d.current))
-    //     .text(d => d.data.name);
+    const label = g.append("g")
+        .attr("pointer-events", "none")
+        .attr("text-anchor", "middle")
+        .style("user-select", "none")
+      .selectAll("text")
+      .data(root.descendants().slice(1))
+      .join("text")
+        .attr("dy", "0.35em")
+        .attr("fill-opacity", d => +labelVisible(d.current))
+        .attr("transform", d => labelTransform(d.current))
+        .text(d => d.data.name);
   
-    // const parent = g.append("circle")
-    //     .datum(root)
-    //     .attr("r", radius)
-    //     .attr("fill", "none")
-    //     .attr("pointer-events", "all")
-    //     .on("click", clicked);
+    const parent = g.append("circle")
+        .datum(root)
+        .attr("r", radius)
+        .attr("fill", "none")
+        .attr("pointer-events", "all")
+        .on("click", clicked);
   
-    // function clicked(event, p) {
-    //   parent.datum(p.parent || root);
+    function clicked(event, p) {
+      parent.datum(p.parent || root);
   
-    //   root.each(d => d.target = {
-    //     x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-    //     x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
-    //     y0: Math.max(0, d.y0 - p.depth),
-    //     y1: Math.max(0, d.y1 - p.depth)
-    //   });
+      root.each(d => d.target = {
+        x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+        x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+        y0: Math.max(0, d.y0 - p.depth),
+        y1: Math.max(0, d.y1 - p.depth)
+      });
   
-    //   const t = g.transition().duration(750);
+      const t = g.transition().duration(750);
   
-    //   // Transition the data on all arcs, even the ones that aren’t visible,
-    //   // so that if this transition is interrupted, entering arcs will start
-    //   // the next transition from the desired position.
-    //   path.transition(t)
-    //       .tween("data", d => {
-    //         const i = d3.interpolate(d.current, d.target);
-    //         return t => d.current = i(t);
-    //       })
-    //     .filter(function(d) {
-    //       return +this.getAttribute("fill-opacity") || arcVisible(d.target);
-    //     })
-    //       .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
-    //       .attrTween("d", d => () => arc(d.current));
+      // Transition the data on all arcs, even the ones that aren’t visible,
+      // so that if this transition is interrupted, entering arcs will start
+      // the next transition from the desired position.
+      path.transition(t)
+          .tween("data", d => {
+            const i = d3.interpolate(d.current, d.target);
+            return t => d.current = i(t);
+          })
+        .filter(function(d) {
+          return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+        })
+          .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+          .attrTween("d", d => () => arc(d.current));
   
-    //   label.filter(function(d) {
-    //       return +this.getAttribute("fill-opacity") || labelVisible(d.target);
-    //     }).transition(t)
-    //       .attr("fill-opacity", d => +labelVisible(d.target))
-    //       .attrTween("transform", d => () => labelTransform(d.current));
-    // }
+      label.filter(function(d) {
+          return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+        }).transition(t)
+          .attr("fill-opacity", d => +labelVisible(d.target))
+          .attrTween("transform", d => () => labelTransform(d.current));
+    }
     
-    // function arcVisible(d) {
-    //   return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
-    // }
+    function arcVisible(d) {
+      return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+    }
   
-    // function labelVisible(d) {
-    //   return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
-    // }
+    function labelVisible(d) {
+      return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+    }
   
-    // function labelTransform(d) {
-    //   const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
-    //   const y = (d.y0 + d.y1) / 2 * radius;
-    //   return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
-    // }
+    function labelTransform(d) {
+      const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
+      const y = (d.y0 + d.y1) / 2 * radius;
+      return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+    }
   
 });
 
@@ -110,7 +114,7 @@ d3.csv('static/data/starships.csv').then(data => {
       (root);
   }
 
-//   color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
+  // color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
 
   format = d3.format(",d")
 
@@ -118,23 +122,12 @@ d3.csv('static/data/starships.csv').then(data => {
 
   radius = width / 6
 
-  d3.csv('/api/v1.0/data').then(data => {
 
-
-    setTimeout(()=>showData(data),5000);
-
-    function showData(data) {
-      // d3.select('body').html('')
-      console.log(data);
-      d3.select('body').html(data)
-    }
-  })
-
-//   arc = d3.arc()
-//     .startAngle(d => d.x0)
-//     .endAngle(d => d.x1)
-//     .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
-//     .padRadius(radius * 1.5)
-//     .innerRadius(d => d.y0 * radius)
-//     .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
-
+  arc = d3.arc()
+    .startAngle(d => d.x0)
+    .endAngle(d => d.x1)
+    .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
+    .padRadius(radius * 1.5)
+    .innerRadius(d => d.y0 * radius)
+    .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
+});
---
+++ Scripts/static/js/starships_2.js
@@ -0,0 +1,349 @@
+function makeResponsive() {
+
+    // if the SVG area isn't empty when the browser loads,
+    // remove it and replace it with a resized version of the chart
+    var svgArea = d3.select("body").select("svg");
+  
+    if (!svgArea.empty()) {
+      svgArea.remove();
+    }
+  
+  var svgWidth = window.innerWidth*0.8;
+  var svgHeight = svgWidth*0.65;
+  
+  // circle and text size are changed based on window resizing
+  var circleR = svgWidth*0.02; 
+  var textsize = parseInt(svgWidth*0.009);
+  
+  var margin = {
+  top: 20,
+  right: 40,
+  bottom: 100,
+  left: 80
+  };
+  
+  var width = svgWidth - margin.left - margin.right;
+  var height = svgHeight - margin.top - margin.bottom;
+  
+  // Create an SVG wrapper, append an SVG group that will hold chart and shift by margins
+  var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+  
+  // Append an SVG group
+  var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  
+  // Initial parameters
+  var chosenXAxis = "passengers";
+  
+  var chosenYAxis ="hyperdrive_rating";
+  
+  // function used for updating x-scale var upon click on axis label
+  function xScale(starshipsData, chosenXAxis) {
+  // create scales
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(starshipsData, d => d[chosenXAxis]) ,
+      d3.max(starshipsData, d => d[chosenXAxis])])
+    .range([0, width]);
+  
+  return xLinearScale;
+  
+  }
+  
+  // function used for updating y-scale var upon click on axis label
+  function yScale(starshipsData, chosenYAxis) {
+  // create scales
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(starshipsData, d => d[chosenYAxis]),
+      d3.max(starshipsData, d => d[chosenYAxis]) ])
+    .range([height, 0]);
+  
+  return yLinearScale;
+  
+  
+  }
+  
+  // function used for updating xAxis var upon click on axis label
+  function renderXAxes(newXScale, xAxis) {
+  var bottomAxis = d3.axisBottom(newXScale);
+  
+  xAxis.transition()
+    .duration(1000)
+    .call(bottomAxis);
+  
+  return xAxis;
+  }
+  
+  // function used for updating yAxis var upon click on axis label
+  function renderYAxes(newYScale, yAxis) {
+  var leftAxis = d3.axisLeft(newYScale);
+  
+  yAxis.transition()
+    .duration(1000)
+    .call(leftAxis);
+  
+  return yAxis;
+  }
+  
+  // function used for updating circles group and text group with a transition to
+  // new circles
+  function renderCircles(circlesGroup, newXScale, newYScale,chosenXAxis,chosenYAxis) {
+  
+  circlesGroup.transition()
+    .duration(1000)
+    .attr("cx", d => newXScale(d[chosenXAxis]))
+    .attr("cy", d => newYScale(d[chosenYAxis]));
+  
+  return circlesGroup;
+  }
+  
+  function renderText(textGroup, newXScale, newYScale,chosenXAxis,chosenYAxis) {
+  
+  textGroup.transition()
+    .duration(1000)
+    .attr("x", d => newXScale(d[chosenXAxis]))
+    .attr("y", d => newYScale(d[chosenYAxis]));
+    
+  
+  return textGroup;
+  }
+  
+  // function used for updating circles group with new tooltip
+  function updateToolTip(chosenXAxis, chosenYAxis,circlesGroup) {
+  
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      if (chosenXAxis === "passengers"){
+        return (`${d.name}<br>${chosenXAxis}: ${d[chosenXAxis]}<br>${chosenYAxis}: ${d[chosenYAxis]}%`); 
+  
+      }    
+      else {
+        return (`${d.name}<br>${chosenXAxis}: ${d[chosenXAxis]}<br>${chosenYAxis}: ${d[chosenYAxis]}%`); 
+      }
+      });
+      
+     
+  circlesGroup.call(toolTip);
+  
+  circlesGroup.on("mouseover", function(d) {
+    toolTip.show(d,this);
+    })
+    .on("mouseout", function(d, index) {
+      toolTip.hide(d);
+    });
+  return circlesGroup;
+  }
+  
+  // Retrieve data from the CSV file and execute everything below
+  d3.csv("../static/data/starship_2.csv").then(function(starshipsData) {
+  // parse data
+  starshipsData.forEach(function(data) {
+  
+    data.passengers = +data.passengers;
+    data.hyperdrive_rating = +data.hyperdrive_rating;
+  
+    data.MGLT = +data.MGLT;
+    data.max_atmosphering_speed = +data.max_atmosphering_speed;
+  
+  });
+  console.log(starshipsData);
+  
+   
+  // xLinearScale function above csv import
+  var xLinearScale = xScale(starshipsData, chosenXAxis);
+  
+  // Create y scale function
+  var yLinearScale = yScale(starshipsData, chosenYAxis);
+  
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+  
+  // append x axis
+  var xAxis = chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+  
+  // append y axis
+  var yAxis = chartGroup.append("g")
+    .classed("y-axis", true)
+    .call(leftAxis);
+  
+  // append initial circles and text
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(starshipsData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d[chosenXAxis]))
+    .attr("cy", d => yLinearScale(d[chosenYAxis]))
+    .attr("r", circleR)
+    .attr("fill", "teal");
+  
+  var textGroup = chartGroup.selectAll("text")
+    .exit()
+    .data(starshipsData)
+    .enter()
+    .append("text")
+    .text(d => d.name)
+    .attr("x", d => xLinearScale(d[chosenXAxis]))
+    .attr("y", d => yLinearScale(d[chosenYAxis]))
+    .attr("font-size", textsize+"px")
+    .attr("text-anchor", "middle")
+    .attr("class","stateText");
+  
+  circlesGroup = updateToolTip(chosenXAxis, chosenYAxis,circlesGroup);
+  
+  
+  // Create group for x-axis labels
+  var labelsGroup = chartGroup.append("g")
+    .attr("transform", `translate(${width / 2}, ${height + 20})`);
+  
+  var passengersLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 20)
+    .attr("class","axis-text-x")
+    .attr("value", "passengers")
+    .classed("active", true)
+    .text("Number of Passengers");
+  
+  var MGLTLabel = labelsGroup.append("text")
+    .attr("x", 0)
+    .attr("y", 40)
+    .attr("class","axis-text-x")
+    .attr("value", "MGLT") 
+    .classed("inactive", true)
+    .text("MGLT");
+  
+  // Create group for y-axis labels
+  
+  var ylabelsGroup = chartGroup.append("g");
+  
+  var hyperdrive_ratingLabel = ylabelsGroup.append("text")
+    .attr("transform", `translate(-40,${height / 2})rotate(-90)`)
+    .attr("dy", "1em")
+    .attr("class","axis-text-y")
+    .classed("axis-text", true)
+    .attr("value", "hyperdrive_rating") 
+    .classed("active", true)
+    .text("Hyperdrive Rating");
+  
+  var max_atmosphering_speedLabel = ylabelsGroup.append("text")
+    .attr("transform", `translate(-60,${height / 2})rotate(-90)`)
+    .attr("dy", "1em")
+    .attr("class","axis-text-y")
+    .attr("value", "max_atmosphering_speed") 
+    .classed("inactive", true)
+    .text("Max Atmosphering Speed");
+  
+  // x axis labels event listener
+  labelsGroup.selectAll(".axis-text-x")
+    .on("click", function() {
+      // get value of selection
+      var value = d3.select(this).attr("value");
+      if (value !== chosenXAxis) {
+  
+        // replaces chosenXAxis with value
+        chosenXAxis = value;
+  
+        console.log(chosenXAxis)
+  
+        // functions here found above csv import
+        // updates x scale for new data
+        xLinearScale = xScale(starshipsData, chosenXAxis);
+        // updates y scale for new data
+        yLinearScale = yScale(starshipsData, chosenYAxis);
+  
+  
+        // updates x axis with transition
+        xAxis = renderXAxes(xLinearScale, xAxis);
+  
+        // updates circles with new x values
+        circlesGroup = renderCircles(circlesGroup, xLinearScale,yLinearScale,chosenXAxis,chosenYAxis);
+  
+        textGroup = renderText(textGroup, xLinearScale,yLinearScale,chosenXAxis,chosenYAxis);
+  
+        // updates tooltips with new info
+        circlesGroup = updateToolTip(chosenXAxis, chosenYAxis,circlesGroup);
+  
+        // changes classes to change bold text
+        if (chosenXAxis === "passengers") {
+          passengersLabel
+            .classed("active", true)
+            .classed("inactive", false);
+          MGLTLabel
+            .classed("active", false)
+            .classed("inactive", true);
+            
+        }
+        else {
+          passengersLabel
+            .classed("active", false)
+            .classed("inactive", true);
+          MGLTLabel
+            .classed("active", true)
+            .classed("inactive", false);
+        }
+    }
+    })
+  
+  
+  // y axis labels event listener
+  ylabelsGroup.selectAll(".axis-text-y")
+    .on("click", function() {
+      var value = d3.select(this).attr("value");
+      if (value !== chosenYAxis) {
+  
+     // replaces chosenYAxis with value
+      chosenYAxis = value;
+  
+      console.log(chosenYAxis)
+  
+     // functions here found above csv import
+     // updates x scale for new data
+     xLinearScale = xScale(starshipsData, chosenXAxis);
+     // updates y scale for new data
+     yLinearScale = yScale(starshipsData, chosenYAxis);
+     // updates Y axis with transition
+     yAxis = renderYAxes(yLinearScale, yAxis);
+  
+     // updates circles with new x values
+     circlesGroup = renderCircles(circlesGroup, xLinearScale,yLinearScale,chosenXAxis,chosenYAxis);
+  
+     textGroup = renderText(textGroup, xLinearScale,yLinearScale,chosenXAxis,chosenYAxis);
+  
+     // updates tooltips with new info
+     circlesGroup = updateToolTip(chosenXAxis, chosenYAxis,circlesGroup);
+  
+       
+     if (chosenYAxis === "hyperdrive_rating") {
+      hyperdrive_ratingLabel
+        .classed("active", true)
+        .classed("inactive", false);
+      max_atmosphering_speedLabel
+        .classed("active", false)
+        .classed("inactive", true);
+  
+      }
+  
+      } else {
+      hyperdrive_ratingLabel
+        .classed("active", false)
+        .classed("inactive", true);
+      max_atmosphering_speedLabel
+        .classed("active", true)
+        .classed("inactive", false);
+       }
+    });
+})
+}
+  // When the browser loads, makeResponsive() is called.
+  makeResponsive();
+  
+  // When the browser window is resized, makeResponsive() is called.
+  d3.select(window).on("resize", makeResponsive);
