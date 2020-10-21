f9499fd6e56ba344be8acf322fb1a74b7efeeb12
carliemazar <azar.carlie@gmail.com>
2020-10-21 19:51:53 -0400

    species update

--- Scripts/static/js/species.js
+++ Scripts/static/js/species.js
@@ -1,25 +1,26 @@
-// Use d3 to pull in data
-d3.csv('../static/data/clean_species.csv').then(function(speciesData) {
-    var names = speciesData.map(data => data.name);
-    var lifespan = speciesData.map(data => data.average_lifespan);
-    var height = speciesData.map(data => data.average_height);
-    var classification = speciesData.map(data => data.classification);
-    var language = speciesData.map(data => data.language);
-    console.log("names", names);
-    console.log("lifespan", lifespan);
-    console.log("height", height);
-    console.log("classification", classification);
-    console.log("language", language);
-
-    // Use d3 to select the table body to append the data to the table
-    d3.select("tbody")
-        .selectAll("tr")
-        .data(speciesData)
-        .enter()
-        .append("tr")
-        .html(function(d) {
-            return `<td>${d.name}</td><td>${d.classification}</td><td>${d.language}</td><td>${d.average_height}</td><td>${d.average_lifespan}</td>`;
-        });
-  }).catch(function(error) {        //catches errors
-    console.log(error);
-
+function makeResponsive() {
+    // Use d3 to pull in data
+    d3.csv('../static/data/clean_species.csv').then(function(speciesData) {
+        var names = speciesData.map(data => data.name);
+        var lifespan = speciesData.map(data => data.average_lifespan);
+        var height = speciesData.map(data => data.average_height);
+        var classification = speciesData.map(data => data.classification);
+        var language = speciesData.map(data => data.language);
+        console.log("names", names);
+        console.log("lifespan", lifespan);
+        console.log("height", height);
+        console.log("classification", classification);
+        console.log("language", language);
+        
+        // Use d3 to select the table body to append the data to the table
+        d3.select("tbody")
+            .selectAll("tr")
+            .data(speciesData)
+            .enter()
+            .append("tr")
+            .html(function(d) {
+                return `<td>${d.name}</td><td>${d.classification}</td><td>${d.language}</td><td>${d.average_height}</td><td>${d.average_lifespan}</td>`;
+            });
+    }).catch(function(error) {        //catches errors
+        console.log(error);
+    });
